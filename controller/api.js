70b00f1d0df1f47654bdb3612db7b0c1f05f4061
aymeric <aymeric.marec@gmail.com>
2025-06-01 22:21:39 +0200

    page eroror when connexion with api doesnt work

--- controller/api.js
+++ controller/api.js
@@ -1,27 +1,32 @@
 let dishes = {}
 
 async function GetDishes() {
-    const response = await fetch(window.env.API_URL+"/api/foods")
-    const data = await response.json()
+    try{
+        const response = await fetch(window.env.API_URL+"/api/foods")
+        const data = await response.json()
 
-    dishes = {
-        "Dish": [],
-        "Starter": [],
-        "Dessert": [],
-        "Drink": [],
-        "Aperitif":[]
+        dishes = {
+            "Dish": [],
+            "Starter": [],
+            "Dessert": [],
+            "Drink": [],
+            "Aperitif":[]
+        }
+
+        data.forEach(item => {
+            dishes[item.type].push({
+                id: item.id,
+                title: item.name,
+                description: item.description,
+                price: parseFloat(item.price),
+                image: window.env.API_URL+item.picture
+            });
+        });
+        return dishes
+    }catch(error){
+        showApiError()
     }
 
-    data.forEach(item => {
-        dishes[item.type].push({
-            id: item.id,
-            title: item.name,
-            description: item.description,
-            price: parseFloat(item.price),
-            image: window.env.API_URL+item.picture
-        });
-    });
-    return dishes
 }
 
 function SendOrder(){
@@ -35,9 +40,21 @@ function SendOrder(){
             "order":cart
         })
     })
-    .then(response => {
-        if (!response.ok) {
-            return 
-        }
-    })
+}
+
+function showApiError() {
+    const overlay = document.querySelector('.api-error-overlay');
+    overlay.classList.add('show');
+}
+
+function hideApiError() {
+    const overlay = document.querySelector('.api-error-overlay');
+    overlay.classList.remove('show');
+}
+
+// Fonction pour réessayer la connexion
+function retryConnection() {
+    hideApiError();
+    // Ici, vous pouvez ajouter la logique pour réessayer la connexion
+    window.location.reload();
 }
